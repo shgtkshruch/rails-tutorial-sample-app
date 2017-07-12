af7730b20cc3cbf3c98e4bdd0974b881ead160e4
shgtkshruch <shgtk.shruch@gmail.com>
2017-07-12 17:12:08 +0900

    Implement Remember me function

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,7 +5,7 @@ module SessionsHelper
   end
 
   # ユーザーを永続的セッションに記憶する
-  def rememer(user)
+  def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
