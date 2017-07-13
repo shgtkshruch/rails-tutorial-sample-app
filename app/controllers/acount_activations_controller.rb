af04d8c2dfbd21ee0b71e568999436c8ed65fb79
shgtkshruch <shgtk.shruch@gmail.com>
2017-07-13 18:02:49 +0900

    Implement activate function

--- app/controllers/acount_activations_controller.rb
+++ app/controllers/acount_activations_controller.rb
@@ -1,2 +1,14 @@
 class AcountActivationsController < ApplicationController
+  def edit
+    user = User.find_by(:email, params[:email])
+    if user && !user.activate? && user.authenticated?(:activation, params[:id])
+      user.update(:activated, true)
+      user.update(:activated_at, Time.zone.now)
+      log_in user
+      flash[:success] = "Account activated"
+      redirect_to user
+    else
+      flash[:danger] = "Invalid activation link"
+      redirect_to root_url
+  end
 end
