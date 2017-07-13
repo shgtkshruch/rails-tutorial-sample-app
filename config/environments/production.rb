8553bce4e3cc0793b5f284370d9434eab4be4cd7
shgtkshruch <shgtk.shruch@gmail.com>
2017-07-13 19:15:12 +0900

    Fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'boiling-castle-50126.herokuapp.com'
-  cnfig.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
