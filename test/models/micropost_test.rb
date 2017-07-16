8a7b191dd326bb070d7d7c6994c345ae85167eef
shgtkshruch <shgtk.shruch@gmail.com>
2017-07-16 12:16:03 +0900

    Tested micropost
    user_id presence
    content length is less than 140

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+    # このコードは慣習的に正しくない
+    @micropost = Micropost.new(content: "Lorem insum", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  test "content chould be at most 140 characters" do
+    @micropost.content = "a" * 141
+    assert_not @micropost.valid?
+  end
 end
