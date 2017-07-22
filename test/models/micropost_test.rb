27255da29491975e06aba3e6d0e37b8112da7225
shgtkshruch <shgtk.shruch@gmail.com>
2017-07-22 14:29:47 +0900

    Improve method to create micropost from user instance

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class MicropostTest < ActiveSupport::TestCase
   def setup
     @user = users(:michael)
-    @micropost = @user.microposts.build(content: "Lorem insum", user_id: @user.id)
+    @micropost = @user.microposts.build(content: "Lorem insum")
   end
 
   test "should be valid" do
