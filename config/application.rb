23bbbcbd956c4049dbff36e26ce7f3c0180ea95e
shgtkshruch <shgtk.shruch@gmail.com>
2017-08-02 21:03:40 +0900

    Enable follow and unfollow with Ajax

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,8 @@ module RailsTutorialSampleApp
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = "Tokyo"
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
+
+    # 認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
